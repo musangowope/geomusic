74f3c5188cb80ed4ae809f1fd031b7ff4f5c4451
musangowope <musangow@gmail.com>
2025-05-31 14:26:31 +0200

    input.tsx: experimental code for exiting

--- geomusic-native/components/ui/input/Input.tsx
+++ geomusic-native/components/ui/input/Input.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  View,
+} from 'react-native';
 import useThemeColors from '@/hooks/useThemeColors';
 import { TextInputProps } from 'react-native/Libraries/Components/TextInput/TextInput';
-import FontSizes from '@/constants/Typography';
+import { FontSizes } from '@/constants/Typography';
 
-const Input = (props: TextInputProps) => {
+interface InputProps extends TextInputProps {
+  dismissKeyboardOnOutsidePress?: boolean;
+}
+
+const Input = (props: InputProps) => {
+  const { dismissKeyboardOnOutsidePress = true, ...restProps } = props;
   const themeColors = useThemeColors();
-  return (
+
+  const inputContent = (
     <TextInput
-      {...props}
+      clearButtonMode={'while-editing'}
+      {...restProps}
       placeholderTextColor={themeColors.textSecondary}
       style={[
         styles.input,
@@ -17,7 +30,19 @@ const Input = (props: TextInputProps) => {
           color: themeColors.textPrimary,
         },
       ]}
-    ></TextInput>
+    />
+  );
+
+  // If dismissKeyboardOnOutsidePress is false, just return the input
+  if (!dismissKeyboardOnOutsidePress) {
+    return inputContent;
+  }
+
+  // Otherwise wrap it in a TouchableWithoutFeedback to dismiss keyboard on outside press
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View>{inputContent}</View>
+    </TouchableWithoutFeedback>
   );
 };
 
