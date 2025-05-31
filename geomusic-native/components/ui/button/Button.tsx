9c45c02c2f79cc52e51f014dc6cf1f1adc57edcc
musangowope <musangow@gmail.com>
2025-05-31 14:26:03 +0200

    Buttontsx: add text style overwrite

--- geomusic-native/components/ui/button/Button.tsx
+++ geomusic-native/components/ui/button/Button.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { IconProps } from '@/components/ui/icon/types';
-import { Button, Pressable, PressableProps, StyleSheet } from 'react-native';
+import {
+  Pressable,
+  PressableProps,
+  StyleProp,
+  StyleSheet,
+  TextStyle,
+  ViewStyle,
+} from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import useThemeColors from '@/hooks/useThemeColors';
+import * as RN from 'react-native';
 
 export interface CtaButtonProps extends PressableProps {
   mode: 'primary' | 'secondary';
   icon?: IconProps;
   text: string;
+  style?: RN.StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
 }
 
 const CtaButton = (props: CtaButtonProps) => {
-  const { text, mode, ...rest } = props;
+  const { text, mode, style, textStyle, ...rest } = props;
   const themeColors = useThemeColors();
 
   const borderColor =
@@ -26,9 +36,11 @@ const CtaButton = (props: CtaButtonProps) => {
           borderColor: borderColor,
           backgroundColor: themeColors.background,
         },
+        // Moving the custom style to the end of the array ensures it will override previous styles
+        style,
       ]}
     >
-      <ThemedText>{text}</ThemedText>
+      <ThemedText style={textStyle}>{text}</ThemedText>
     </Pressable>
   );
 };
@@ -39,9 +51,10 @@ const styles = StyleSheet.create({
   button: {
     paddingLeft: 15,
     paddingRight: 15,
-    paddingTop: 8,
-    paddingBottom: 8,
+    paddingTop: 6,
+    paddingBottom: 6,
     borderWidth: 1,
     borderRadius: 18,
+    alignSelf: 'flex-start', // This makes the button width auto-adjust to content
   },
 });
